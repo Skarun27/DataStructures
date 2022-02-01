5f0b16080d6ecfa6d12f57915aed098470fe722a
shubhamkarun <sk4074@g.rit.edu>
2022-02-01 06:56:53 -0500

    Feb 01 leetcode

--- src/SDESheet/ArraysPartIII/SearchInA2DMatrix.java
+++ src/SDESheet/ArraysPartIII/SearchInA2DMatrix.java
@@ -17,7 +17,7 @@ public class SearchInA2DMatrix {
     }
 
     public static void main(String[] args) {
-        int[][] arr = new int[][]{{1,3,5,7},{10,11,16,20}{23,30,34,60}};
+        int[][] arr = new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}};
         System.out.println(searchMatrix(arr, 3));
     }
 }
