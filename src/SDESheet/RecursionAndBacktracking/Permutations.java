0af352aacd16bd92944801f438f1746ebbbb6244
shubhamkarun <sk4074@g.rit.edu>
2022-02-07 08:44:46 -0500

    Feb 07 leetcode

---
+++ src/SDESheet/RecursionAndBacktracking/Permutations.java
@@ -0,0 +1,40 @@
+package SDESheet.RecursionAndBacktracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> list = new ArrayList();
+        List<Integer> sublist = new ArrayList();
+        permutations(0, sublist, list, nums);
+        return list;
+    }
+
+    static void permutations(int ind, List<Integer> sublist, List<List<Integer>> list, int[] nums) {
+        if(ind == nums.length) {
+            sublist = new ArrayList<>();
+            for(int i=0; i<nums.length; i++) {
+                sublist.add(nums[i]);
+            }
+            list.add(sublist);
+            return;
+        }
+        for(int i=ind; i<nums.length; i++) {
+            swap(ind, i, nums);
+            permutations(ind+1, sublist, list, nums);
+            swap(ind, i, nums);
+        }
+    }
+
+    static void swap(int i, int j, int[] nums) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3};
+        System.out.println(permute(nums));
+    }
+}
