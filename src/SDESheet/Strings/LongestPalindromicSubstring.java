fe163fc97b9fd45ad80cd45c7ce2ff09625c67b1
shubhamkarun <sk4074@g.rit.edu>
2022-02-16 09:55:13 -0500

    Feb 16 leetcode

---
+++ src/SDESheet/Strings/LongestPalindromicSubstring.java
@@ -0,0 +1,30 @@
+package SDESheet.Strings;
+
+public class LongestPalindromicSubstring {
+    public static String longestPalindrome(String s) {
+        int start = 0;
+        int end = 0;
+        for(int i=0; i<s.length(); i++) {
+            int len1 = expandFromCenter(s, i, i);
+            int len2 = expandFromCenter(s, i, i+1);
+            int len = Math.max(len1, len2);
+            if(len > end - start) {
+                start = i - (len-1)/2;
+                end = i + (len)/2;
+            }
+        }
+        return s.substring(start, end+1);
+    }
+
+    public static int expandFromCenter(String s, int start, int end) {
+        while(start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
+            start--;
+            end++;
+        }
+        return end - start - 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("babad"));
+    }
+}
