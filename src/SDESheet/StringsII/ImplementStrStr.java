588cdd3da894306f982bb82d09e5f2a6eda3c44a
shubhamkarun <sk4074@g.rit.edu>
2022-02-17 22:36:35 -0500

    Feb 17 leetcode

---
+++ src/SDESheet/StringsII/ImplementStrStr.java
@@ -0,0 +1,17 @@
+package SDESheet.StringsII;
+
+public class ImplementStrStr {
+    public static int strStr(String haystack, String needle) {
+        for (int i = 0; ; i++) {
+            for (int j = 0; ; j++) {
+                if (j == needle.length()) return i;
+                if (i + j == haystack.length()) return -1;
+                if (needle.charAt(j) != haystack.charAt(i + j)) break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        strStr("hello", "ll");
+    }
+}
---
+++ src/SDESheet/StringsII/ValidAnagrams.java
@@ -0,0 +1,4 @@
+package SDESheet.StringsII;
+
+public class ValidAnagrams {
+}
