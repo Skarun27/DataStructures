798077131f2d64f4307aa71378a4281a893da86d
shubhamkarun <sk4074@g.rit.edu>
2022-02-19 13:02:21 -0500

    Feb 17 leetcode

--- src/SDESheet/BinaryTree/PreOrderTraversal.java
+++ src/SDESheet/BinaryTree/PreOrderTraversal.java
@@ -3,23 +3,24 @@ package SDESheet.BinaryTree;
 import java.util.ArrayList;
 import java.util.List;
 
-public class InorderTraversal {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList();
-        inorder(root, list);
+public class PreOrderTraversal {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        recursive(root, list);
         return list;
     }
 
-    public void inorder(TreeNode root, List<Integer> list) {
+    public void recursive(TreeNode root, List<Integer> list) {
         if(root != null) {
-            inorder(root.left, list);
             list.add(root.val);
-            inorder(root.right, list);
+            recursive(root.left, list);
+            recursive(root.right, list);
         }
     }
 
-     //Definition for a binary tree node.
-     private class TreeNode {
+    //Definition for a binary tree node.
+    private class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
