c7115534232b0e85a19128f36a45a016fe6ad00b
shubhamkarun <sk4074@g.rit.edu>
2022-05-24 09:24:43 -0400

    Nqueen

--- src/SDESheet/RecursionAndBacktracking/NQueen.java
+++ src/SDESheet/RecursionAndBacktracking/NQueen.java
@@ -1,69 +1,55 @@
 package SDESheet.RecursionAndBacktracking;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashSet;
 
 public class NQueen {
 
-    public static List <List< String >> solveNQueens(int n) {
-        char[][] board = new char[n][n];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                board[i][j] = '.';
-        List < List < String >> res = new ArrayList< List < String >>();
-        int leftRow[] = new int[n];
-        int upperDiagonal[] = new int[2 * n - 1];
-        int lowerDiagonal[] = new int[2 * n - 1];
-        solve(0, board, res, leftRow, lowerDiagonal, upperDiagonal);
-        return res;
+    public static int totalNQueens(int n) {
+        HashSet<Integer> col = new HashSet<>();
+        HashSet<Integer> diagonals = new HashSet<>();
+        HashSet<Integer> antiDiagonals = new HashSet<>();
+        return solveNQueens(n, 0, col, diagonals, antiDiagonals);
     }
-
-
-
-    static void solve(int col, char[][] board, List < List < String >> res, int leftRow[], int lowerDiagonal[], int upperDiagonal[]) {
-        if (col == board.length) {
-            res.add(construct(board));
-            return;
-        }
-
-        for (int row = 0; row < board.length; row++) {
-            if (leftRow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[board.length - 1 + col - row] == 0) {
-                board[row][col] = 'Q';
-                leftRow[row] = 1;
-                lowerDiagonal[row + col] = 1;
-                upperDiagonal[board.length - 1 + col - row] = 1;
-                solve(col + 1, board, res, leftRow, lowerDiagonal, upperDiagonal);
-                board[row][col] = '.';
-                leftRow[row] = 0;
-                lowerDiagonal[row + col] = 0;
-                upperDiagonal[board.length - 1 + col - row] = 0;
+    
+    public static int solveNQueens(int n, int row, HashSet<Integer> columns, HashSet<Integer> diagonals, HashSet<Integer> antiDiagonals) {
+        
+        if(row == n)
+            return 1;
+        
+        int count = 0;
+        
+        for(int col=0; col<n; col++) {
+            if(isValidPos(row, col, columns, diagonals, antiDiagonals)) {
+                placeQueen(row, col, columns, diagonals, antiDiagonals);
+                count += solveNQueens(n, row+1, columns, diagonals, antiDiagonals);
+                removeQueens(row, col, columns, diagonals, antiDiagonals);
             }
         }
+        return count;
+    }
+    
+    public static boolean isValidPos(int row, int col, HashSet<Integer> columns, HashSet<Integer> diagonals, HashSet<Integer> antiDiagonals) {
+        
+        if(columns.contains(col) || diagonals.contains(row-col) || antiDiagonals.contains(row+col))
+            return false;
+        return true;
+    }
+    
+    public static void placeQueen(int row, int col, HashSet<Integer> columns, HashSet<Integer> diagonals, HashSet<Integer> antiDiagonals) {
+        
+        columns.add(col);
+        diagonals.add(row-col);
+        antiDiagonals.add(row+col);
+    }
+    
+    public static void removeQueens(int row, int col, HashSet<Integer> columns, HashSet<Integer> diagonals, HashSet<Integer> antiDiagonals) {
+        columns.remove(col);
+        diagonals.remove(row-col);
+        antiDiagonals.remove(row+col);
     }
 
-
-    static List < String > construct(char[][] board) {
-        List < String > res = new LinkedList< String >();
-        for (int i = 0; i < board.length; i++) {
-            String s = new String(board[i]);
-            res.add(s);
-        }
-        return res;
-    }
-
-    public static void main(String args[]) {
-        int N = 4;
-        List < List < String >> queen = solveNQueens(N);
-        int i = 1;
-        for (List < String > it: queen) {
-            System.out.println("Arrangement " + i);
-            for (String s: it) {
-                System.out.println(s);
-            }
-            System.out.println();
-            i += 1;
-        }
-
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(totalNQueens(n));
     }
 }
