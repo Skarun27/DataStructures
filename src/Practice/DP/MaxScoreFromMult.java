5634d27be10c8be0801bfb00e45cb1ef780789f3
shubhamkarun <sk4074@g.rit.edu>
2022-06-01 10:49:37 -0400

    max score from mult

---
+++ src/Practice/DP/MaxScoreFromMult.java
@@ -0,0 +1,33 @@
+package Practice.DP;
+
+class MaxScoreFromMult {
+        
+    int[] nums;
+    int[] multipliers;
+    int[][] maxScore;
+
+    public int maximumScore(int[] nums, int[] multipliers) {
+        
+        this.nums = nums;
+        this.multipliers = multipliers;
+        int m = multipliers.length;
+        maxScore = new int[m][m];
+        
+        return dp(0, 0);
+    }
+
+    public int dp(int i, int left) {
+        
+        if(i == multipliers.length)
+            return 0;
+        
+        int right = nums.length - 1 - (i - left);
+        
+        if(maxScore[i][left] == 0) {
+            maxScore[i][left] = Math.max(dp(i+1, left+1) + multipliers[i] * nums[left] , 
+                                dp(i+1, left) + multipliers[i] * nums[right]);
+        }
+        
+        return maxScore[i][left];     
+    }
+}
