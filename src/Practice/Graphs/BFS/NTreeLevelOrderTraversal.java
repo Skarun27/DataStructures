5f0efcee54ed034bd9ee04bdd0bdbc620ecf73d9
shubhamkarun <sk4074@g.rit.edu>
2022-06-28 11:11:43 -0400

    Kruskals Algorithm

--- src/Practice/Graphs/BFS/NTreeLevelOrderTraversal.java
+++ src/Practice/Graphs/BFS/NTreeLevelOrderTraversal.java
@@ -35,3 +35,20 @@ public class NTreeLevelOrderTraversal {
         return traversal;
     }
 }
+
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
